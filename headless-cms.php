8bd1324f2bc8c680c628e7e876708a5cf30a459a
Imran <imranhsayed@gmail.com>
2021-04-25 22:08:28 +0530

    Update readme

--- headless-cms.php
+++ headless-cms.php
@@ -34,32 +34,3 @@ function headless_cms_features_plugin_loader() {
 }
 
 headless_cms_features_plugin_loader();
-
-
-
-//function hello() {
-//
-//	if ( !class_exists('WooCommerce') ) {
-//		return [];
-//	}
-//
-//	$countries_with_country_codes = WC()->countries;
-//	$all_countries = WC()->countries->get_states();
-//	$countries_with_states = [];
-//
-//	if ( empty( $all_countries ) && !is_array( $all_countries ) ) {
-//		return [];
-//	}
-//
-//	foreach ( $all_countries as $country_code => $states ) {
-//		if ( ! empty( $states ) ) {
-//			$countries_with_states[$country_code] = $country_code;
-//		}
-//	}
-//
-//	echo '<pre/>';
-//	print_r($countries_with_country_codes);
-//	wp_die();
-//}
-//
-//add_action('init', 'hello' );
