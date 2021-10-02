432acf1addbe51a0734925bd3414b86c6df331d4
Imran <imranhsayed@gmail.com>
2021-10-02 12:45:51 +0530

    Add doc comment

--- inc/classes/api/class-api-settings.php
+++ inc/classes/api/class-api-settings.php
@@ -10,7 +10,7 @@ namespace Headless_CMS\Features\Inc\Api;
 use Headless_CMS\Features\Inc\Traits\Singleton;
 
 /**
- * Class Header_Footer_Api
+ * Class API_Settings
  */
 class API_Settings {
 
@@ -30,32 +30,73 @@ class API_Settings {
 	 */
 	protected function setup_hooks() {
 
+		// If the site url is same as home url, then don't make any updates.
+		if ( get_site_url() === get_home_url() ) {
+			return null;
+		}
+
+		/**
+		 * Update site and home URLs for rest api call.
+		 */
 		add_filter( 'rest_url', [ $this, 'force_update_rest_url' ] );
 
+		/**
+		 * Update site and home URLs for rest api call for WooCommerce.
+		 *
+		 * @see https://github.com/woocommerce/woocommerce/blob/0c44ab857a9b041727ab8c16fd182ee9b700818e/includes/class-wc-rest-authentication.php#L41
+		 */
 		add_action( 'determine_current_user', [ $this, 'update_site_and_home_url' ], 1, 1 );
+
+		/**
+		 * Reset the site and home URLs after authentication rest api call for WooCommerce.
+		 */
 		add_action( 'determine_current_user', [ $this, 'reset_site_and_home_url' ], 20, 1 );
 
 	}
 
+	/**
+	 * Update the home and site url for the REST api authentication call.
+	 *
+	 * This is required because check_oauth_signature() of WooCommerce uses get_home_url to verify the signature.
+	 * At that point, the correct site url is not available. So we modify the URL to the backend url and then
+	 * later reset it back after the function completes it's work.
+	 *
+	 * @param int $user_id User ID.
+	 *
+	 * @return mixed
+	 */
 	function update_site_and_home_url( $user_id ) {
 
-		add_filter( 'home_url', [ $this, "update_urls" ], 1, 2 );
-		add_filter( 'site_url', [ $this, "update_urls" ], 1, 2 );
+		add_filter( 'home_url', [ $this, 'update_urls_callback' ], 1, 2 );
+		add_filter( 'site_url', [ $this, 'update_urls_callback' ], 1, 2 );
 
 		return $user_id;
 	}
 
-	function update_urls( $url, $path ) {
+	/**
+	 * Update URL Callback.
+	 *
+	 * @param $url
+	 * @param $path
+	 *
+	 * @return string
+	 */
+	public function update_urls_callback( $url, $path ) {
 
 		$url = get_option('siteurl');
 
 		return $url . $path;
 	}
 
-	function reset_site_and_home_url( $user_id ) {
+	/**
+	 * Reset the site and home URLs after authentication rest api call.
+	 *
+	 * @param int $user_id User id
+	 */
+	public function reset_site_and_home_url( $user_id ) {
 
-		remove_filter( 'home_url', [ $this, "update_urls" ], 1 );
-		remove_filter( 'site_url', [ $this, "update_urls" ], 1 );
+		remove_filter( 'home_url', [ $this, 'update_urls_callback' ], 1 );
+		remove_filter( 'site_url', [ $this, 'update_urls_callback' ], 1 );
 
 		return $user_id;
 	}
