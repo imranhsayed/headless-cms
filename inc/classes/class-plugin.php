d7b0b401984d774e94cf8747cc3648ca9bce7ff6
Imran <imranhsayed@gmail.com>
2022-09-10 09:56:24 +0530

    Add rest api endpoint for woocommerce countries

--- inc/classes/class-plugin.php
+++ inc/classes/class-plugin.php
@@ -14,6 +14,7 @@ use Headless_CMS\Features\Inc\Api\Header_Footer_Api;
 use Headless_CMS\Features\Inc\Api\Home_Page;
 use Headless_CMS\Features\Inc\Api\Wc_Cart;
 use Headless_CMS\Features\Inc\Api\Post_By_Tax;
+use Headless_CMS\Features\Inc\Api\Wc_Countries;
 use Headless_CMS\Features\Inc\Mutations\Add_Wishlist;
 use Headless_CMS\Features\Inc\Mutations\Delete_Wishlist;
 use Headless_CMS\Features\Inc\Queries\Get_Wishlist;
@@ -53,6 +54,7 @@ class Plugin {
 		Home_Page::get_instance();
 		Post_By_Tax::get_instance();
 		Wc_Cart::get_instance();
+		Wc_Countries::get_instance();
 
 		// Queries.
 		Header_Footer_Schema::get_instance();
